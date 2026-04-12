68c9066d049d6d642aa3dcb9b6d39d77ba83c642
sunnyhan0317 <yihan970317@gmail.com>
2026-04-12 17:39:58 +0800

    add path in server.js

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,10 @@ app.use('/api/ai', aiRoutes);
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 // Connect to MongoDB
